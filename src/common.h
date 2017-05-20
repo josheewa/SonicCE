fa94cc821be6ebc341cf3a055d7faec1fa68cc81
Tyulis <gmnrt@laposte.net>
2017-05-20 19:28:05 +0200

    Create common.h

---
+++ src/common.h
@@ -0,0 +1,23 @@
+#ifndef _SONICCE_COMMON_H_
+#define _SONICCE_COMMON_H_
+/* Keep these headers */
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <tice.h>
+
+/* Standard headers - it's recommended to leave them included */
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/* Other available headers */
+#include <lib/ce/keypadc.h>
+#include <lib/ce/graphx.h>
+#include <lib/ce/fileioc.h>
+#include <decompress.h>
+/* Project headers */
+#include "constants.h"
+#include "structs.h"
+#include "physic.h"
+#endif
