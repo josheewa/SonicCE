c83b12c470ac09af385cc35ae0048d51cdc8351f
Tyulis <gmnrt@laposte.net>
2017-05-25 00:55:04 +0200

    added test levels and levels utilities

--- src/levels/csv2asm.py
+++ src/levels/csv2asm.py
@@ -1,1 +1,24 @@
+import sys
 
+args=sys.argv[1:len(sys.argv)]
+for fn in args:
+	f=open(fn+".csv","r")
+	c=f.read().splitlines()
+	f.close()
+	unsigned=[]
+	for ln in c:
+		l=[int(el) for el in ln.split(",")]
+		for i in range(0,len(l)):
+			l[i]=l[i] if l[i]!=-1 else 0
+		unsigned.append(l)
+	final=""
+	for ln in unsigned:
+		final+=" db "
+		l=[("%xh"%el).zfill(3) for el in ln]
+		final+=",".join(l)
+		final+="\n"
+	f=open(fn+"_data.ez80","w")
+	f.write(final)
+	f.close()
+	print("Converted "+fn)
+print("Finished")
