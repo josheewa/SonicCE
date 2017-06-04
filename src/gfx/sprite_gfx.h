3f43c59946307d46df803d39ccee783e4353a5c3
Tyulis <gmnrt@laposte.net>
2017-06-04 16:21:53 +0200

    major graphics updates

--- src/gfx/sprite_gfx.h
+++ src/gfx/sprite_gfx.h
@@ -4,23 +4,21 @@
 #define sprite_gfx_H
 #include <stdint.h>
 
-extern uint8_t sonic_standby_compressed[219];
-extern uint8_t sonic_look_up_compressed[200];
-extern uint8_t sonic_look_down_compressed[171];
-extern uint8_t sonic_ball_compressed[96];
-extern uint8_t sonic_hit_compressed[246];
+extern uint8_t sonic_standby_compressed[233];
+extern uint8_t sonic_ball_compressed[106];
+extern uint8_t sonic_hit_compressed[247];
 extern uint8_t sonic_dead_compressed[267];
-extern uint8_t sonic_walk_1_compressed[255];
-extern uint8_t sonic_walk_2_compressed[236];
-extern uint8_t sonic_walk_3_compressed[261];
-extern uint8_t sonic_walk_wall_1_compressed[254];
-extern uint8_t sonic_walk_wall_2_compressed[225];
-extern uint8_t sonic_walk_wall_3_compressed[246];
-extern uint8_t sonic_run_1_compressed[221];
-extern uint8_t sonic_run_2_compressed[214];
-extern uint8_t sonic_run_wall_1_compressed[248];
-extern uint8_t sonic_run_wall_2_compressed[229];
-extern uint8_t sonic_push_compressed[220];
+extern uint8_t sonic_walk_1_compressed[260];
+extern uint8_t sonic_walk_2_compressed[250];
+extern uint8_t sonic_walk_3_compressed[265];
+extern uint8_t sonic_walk_wall_1_compressed[250];
+extern uint8_t sonic_walk_wall_2_compressed[228];
+extern uint8_t sonic_walk_wall_3_compressed[244];
+extern uint8_t sonic_run_1_compressed[231];
+extern uint8_t sonic_run_2_compressed[222];
+extern uint8_t sonic_run_wall_1_compressed[251];
+extern uint8_t sonic_run_wall_2_compressed[231];
+extern uint8_t sonic_push_compressed[231];
 extern uint8_t ring_compressed[56];
 extern uint8_t checkpoint_compressed[135];
 extern uint8_t checkpoint_ok_compressed[140];
